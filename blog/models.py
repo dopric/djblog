9db0df15effae61b943e935c14f7d69c53cebb4b
Dragan Opric <opric.dragan@gmail.com>
2020-12-02 08:15:02 +0100

    blog routes registred

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -32,3 +33,10 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+        args=[self.publis.year,
+        self.publish.month,
+        self.publish.day,
+        self.slug  ])
