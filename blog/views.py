16d3229608d68a9028a7005f8a7859d4657d75a3
Dragan Opric <opric.dragan@gmail.com>
2020-12-03 08:17:52 +0100

    pagination and css added

--- blog/views.py
+++ blog/views.py
@@ -6,16 +6,21 @@ from django.core.paginator import  Paginator, PageNotAnInteger, EmptyPage
 
 
 def post_list(request):
-    posts = Post.published.all()
-    paginator = Paginator(posts, 3)
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3)  # 3 posts in each page
+    page = request.GET.get('page')
     try:
-        page = request.GET.get('page')
+        posts = paginator.page(page)
     except PageNotAnInteger:
+        # If page is not an integer deliver the first page
         posts = paginator.page(1)
     except EmptyPage:
+        # If page is out of range deliver last page of results
         posts = paginator.page(paginator.num_pages)
-
-    return render(request, "blog/post/list.html", {"posts": posts, "page": page})
+    return render(request,
+                  'blog/post/list.html',
+                  {'page': page,
+                   'posts': posts})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
