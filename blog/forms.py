d0c044b79a9064de159b72ed93e9cdb1a7d52c60
Dragan Opric <opric.dragan@gmail.com>
2020-12-04 16:37:03 +0100

    post form added

---
+++ blog/forms.py
@@ -0,0 +1,9 @@
+
+
+from django import forms
+
+class SharePostForm(forms.Form):
+    name = forms.CharField(max_length=250)
+    email = forms.EmailField()
+    to = forms.EmailField()
+    comments = forms.CharField(required=False, widget=forms.Textarea)
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.core.paginator import  Paginator, PageNotAnInteger, EmptyPage
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.views.generic import ListView
+from .forms import SharePostForm
+
 
 # Create your views here.
 
@@ -23,15 +25,31 @@ def post_list(request):
                   {'page': page,
                    'posts': posts})
 
+
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
-                                   status='published',
-                                   publish__year=year,
-                                   publish__month=month,
-                                   publish__day=day)
+                             status='published',
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    if request.method == 'POST':
+        form = SharePostForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            print("data", data)
+        else:
+            pass
+    else:
+        form = SharePostForm()
+    return render(request, 'blog/post/share.html', {"post": post, 'form': form})
+
 
 class PostListView(ListView):
     queryset = Post.published.all()
